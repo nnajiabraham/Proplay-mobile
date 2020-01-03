9a34103785b9ab6955df14c924c3efb8400b692d
Abraham Nnaji <nnajiabraham@gmail.com>
2020-01-03 17:22:03 -0600

    removed search icon

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,15 +1,7 @@
 import React from 'react';
-import {
-  View,
-  StatusBar,
-  StyleSheet,
-  Dimensions,
-  TouchableOpacity,
-} from 'react-native';
+import {View, StatusBar, StyleSheet, Dimensions} from 'react-native';
 import SafeViewWrapper from '../../components/SafeViewWrapper';
 import VideoFlatList from '../../components/VideoFlatList';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import {useNavigation} from 'react-navigation-hooks';
 
 const styles = StyleSheet.create({
   container: {
@@ -20,27 +12,12 @@ const styles = StyleSheet.create({
     height: Dimensions.get('screen').height,
     width: Dimensions.get('screen').width,
   },
-  search: {
-    position: 'absolute',
-    zIndex: 5,
-    top: 0,
-    left: '80%',
-    marginTop: StatusBar.currentHeight ? 26 : 52,
-  },
 });
 
 const Home = () => {
-  const {navigate} = useNavigation();
-
   return (
     <SafeViewWrapper>
       <StatusBar barStyle="light-content" />
-      <TouchableOpacity
-        style={styles.search}
-        onPress={() => navigate('Search')}
-      >
-        <Icon name={'magnify'} color={'#fff'} size={35} />
-      </TouchableOpacity>
       <View style={styles.container}>
         <VideoFlatList />
       </View>
